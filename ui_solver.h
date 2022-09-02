b7665bb7631d567f35f3cf6a61190fa645866517
dewasser <60730368+dewasser@users.noreply.github.com>
2022-09-02 10:52:13 +0200

    Create ui_solver.h

---
+++ ui_solver.h
@@ -0,0 +1,279 @@
+/********************************************************************************
+** Form generated from reading UI file 'solver.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_SOLVER_H
+#define UI_SOLVER_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_solver
+{
+public:
+    QWidget *centralwidget;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QLineEdit *lineEdit;
+    QWidget *horizontalLayoutWidget_3;
+    QHBoxLayout *horizontalLayout_3;
+    QLineEdit *lineEdit_3;
+    QWidget *horizontalLayoutWidget_2;
+    QHBoxLayout *horizontalLayout_2;
+    QLineEdit *lineEdit_2;
+    QWidget *horizontalLayoutWidget_4;
+    QHBoxLayout *horizontalLayout_4;
+    QLineEdit *lineEdit_4;
+    QWidget *horizontalLayoutWidget_5;
+    QHBoxLayout *horizontalLayout_5;
+    QLineEdit *lineEdit_5;
+    QWidget *horizontalLayoutWidget_6;
+    QHBoxLayout *horizontalLayout_6;
+    QLineEdit *lineEdit_6;
+    QPlainTextEdit *plainTextEdit;
+    QPushButton *pushButton;
+    QWidget *horizontalLayoutWidget_7;
+    QHBoxLayout *horizontalLayout_7;
+    QLineEdit *lineEdit_7;
+    QWidget *horizontalLayoutWidget_8;
+    QHBoxLayout *horizontalLayout_8;
+    QLineEdit *lineEdit_8;
+    QTextEdit *textEdit;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *ChartL;
+    QPushButton *pushButton_2;
+    QWidget *horizontalLayoutWidget_9;
+    QHBoxLayout *horizontalLayout_9;
+    QLineEdit *lineEdit_9;
+    QLabel *label;
+    QTextBrowser *textBrowser;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *solver)
+    {
+        if (solver->objectName().isEmpty())
+            solver->setObjectName(QString::fromUtf8("solver"));
+        solver->resize(800, 600);
+        centralwidget = new QWidget(solver);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        horizontalLayoutWidget = new QWidget(centralwidget);
+        horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(110, 140, 141, 41));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        lineEdit = new QLineEdit(horizontalLayoutWidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+        horizontalLayoutWidget_3 = new QWidget(centralwidget);
+        horizontalLayoutWidget_3->setObjectName(QString::fromUtf8("horizontalLayoutWidget_3"));
+        horizontalLayoutWidget_3->setGeometry(QRect(110, 180, 141, 41));
+        horizontalLayout_3 = new QHBoxLayout(horizontalLayoutWidget_3);
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setContentsMargins(0, 0, 0, 0);
+        lineEdit_3 = new QLineEdit(horizontalLayoutWidget_3);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+
+        horizontalLayout_3->addWidget(lineEdit_3);
+
+        horizontalLayoutWidget_2 = new QWidget(centralwidget);
+        horizontalLayoutWidget_2->setObjectName(QString::fromUtf8("horizontalLayoutWidget_2"));
+        horizontalLayoutWidget_2->setGeometry(QRect(110, 230, 141, 41));
+        horizontalLayout_2 = new QHBoxLayout(horizontalLayoutWidget_2);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        lineEdit_2 = new QLineEdit(horizontalLayoutWidget_2);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+
+        horizontalLayout_2->addWidget(lineEdit_2);
+
+        horizontalLayoutWidget_4 = new QWidget(centralwidget);
+        horizontalLayoutWidget_4->setObjectName(QString::fromUtf8("horizontalLayoutWidget_4"));
+        horizontalLayoutWidget_4->setGeometry(QRect(110, 280, 141, 41));
+        horizontalLayout_4 = new QHBoxLayout(horizontalLayoutWidget_4);
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
+        lineEdit_4 = new QLineEdit(horizontalLayoutWidget_4);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+
+        horizontalLayout_4->addWidget(lineEdit_4);
+
+        horizontalLayoutWidget_5 = new QWidget(centralwidget);
+        horizontalLayoutWidget_5->setObjectName(QString::fromUtf8("horizontalLayoutWidget_5"));
+        horizontalLayoutWidget_5->setGeometry(QRect(110, 90, 141, 41));
+        horizontalLayout_5 = new QHBoxLayout(horizontalLayoutWidget_5);
+        horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
+        horizontalLayout_5->setContentsMargins(0, 0, 0, 0);
+        lineEdit_5 = new QLineEdit(horizontalLayoutWidget_5);
+        lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
+
+        horizontalLayout_5->addWidget(lineEdit_5);
+
+        horizontalLayoutWidget_6 = new QWidget(centralwidget);
+        horizontalLayoutWidget_6->setObjectName(QString::fromUtf8("horizontalLayoutWidget_6"));
+        horizontalLayoutWidget_6->setGeometry(QRect(110, 330, 141, 41));
+        horizontalLayout_6 = new QHBoxLayout(horizontalLayoutWidget_6);
+        horizontalLayout_6->setObjectName(QString::fromUtf8("horizontalLayout_6"));
+        horizontalLayout_6->setContentsMargins(0, 0, 0, 0);
+        lineEdit_6 = new QLineEdit(horizontalLayoutWidget_6);
+        lineEdit_6->setObjectName(QString::fromUtf8("lineEdit_6"));
+
+        horizontalLayout_6->addWidget(lineEdit_6);
+
+        plainTextEdit = new QPlainTextEdit(centralwidget);
+        plainTextEdit->setObjectName(QString::fromUtf8("plainTextEdit"));
+        plainTextEdit->setGeometry(QRect(0, 0, 104, 421));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(260, 0, 121, 31));
+        horizontalLayoutWidget_7 = new QWidget(centralwidget);
+        horizontalLayoutWidget_7->setObjectName(QString::fromUtf8("horizontalLayoutWidget_7"));
+        horizontalLayoutWidget_7->setGeometry(QRect(110, 40, 141, 41));
+        horizontalLayout_7 = new QHBoxLayout(horizontalLayoutWidget_7);
+        horizontalLayout_7->setObjectName(QString::fromUtf8("horizontalLayout_7"));
+        horizontalLayout_7->setContentsMargins(0, 0, 0, 0);
+        lineEdit_7 = new QLineEdit(horizontalLayoutWidget_7);
+        lineEdit_7->setObjectName(QString::fromUtf8("lineEdit_7"));
+
+        horizontalLayout_7->addWidget(lineEdit_7);
+
+        horizontalLayoutWidget_8 = new QWidget(centralwidget);
+        horizontalLayoutWidget_8->setObjectName(QString::fromUtf8("horizontalLayoutWidget_8"));
+        horizontalLayoutWidget_8->setGeometry(QRect(110, 0, 141, 41));
+        horizontalLayout_8 = new QHBoxLayout(horizontalLayoutWidget_8);
+        horizontalLayout_8->setObjectName(QString::fromUtf8("horizontalLayout_8"));
+        horizontalLayout_8->setContentsMargins(0, 0, 0, 0);
+        lineEdit_8 = new QLineEdit(horizontalLayoutWidget_8);
+        lineEdit_8->setObjectName(QString::fromUtf8("lineEdit_8"));
+
+        horizontalLayout_8->addWidget(lineEdit_8);
+
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        textEdit->setGeometry(QRect(390, 0, 401, 61));
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(269, 69, 531, 301));
+        ChartL = new QVBoxLayout(verticalLayoutWidget);
+        ChartL->setObjectName(QString::fromUtf8("ChartL"));
+        ChartL->setContentsMargins(0, 0, 0, 0);
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(260, 30, 121, 31));
+        horizontalLayoutWidget_9 = new QWidget(centralwidget);
+        horizontalLayoutWidget_9->setObjectName(QString::fromUtf8("horizontalLayoutWidget_9"));
+        horizontalLayoutWidget_9->setGeometry(QRect(110, 380, 141, 41));
+        horizontalLayout_9 = new QHBoxLayout(horizontalLayoutWidget_9);
+        horizontalLayout_9->setObjectName(QString::fromUtf8("horizontalLayout_9"));
+        horizontalLayout_9->setContentsMargins(0, 0, 0, 0);
+        lineEdit_9 = new QLineEdit(horizontalLayoutWidget_9);
+        lineEdit_9->setObjectName(QString::fromUtf8("lineEdit_9"));
+
+        horizontalLayout_9->addWidget(lineEdit_9);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(420, 380, 101, 31));
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+        textBrowser->setGeometry(QRect(350, 380, 61, 31));
+        solver->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(solver);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 24));
+        solver->setMenuBar(menubar);
+        statusbar = new QStatusBar(solver);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        solver->setStatusBar(statusbar);
+
+        retranslateUi(solver);
+
+        QMetaObject::connectSlotsByName(solver);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *solver)
+    {
+        solver->setWindowTitle(QCoreApplication::translate("solver", "solver", nullptr));
+        lineEdit->setText(QCoreApplication::translate("solver", "1", nullptr));
+        lineEdit_3->setText(QCoreApplication::translate("solver", "1e3", nullptr));
+        lineEdit_2->setText(QCoreApplication::translate("solver", "1e-9", nullptr));
+        lineEdit_4->setText(QCoreApplication::translate("solver", "1e-9", nullptr));
+        lineEdit_5->setText(QCoreApplication::translate("solver", "0.1", nullptr));
+        lineEdit_6->setText(QCoreApplication::translate("solver", "1e9", nullptr));
+        plainTextEdit->setPlainText(QCoreApplication::translate("solver", "Is\n"
+"\n"
+"\n"
+"nvt(Ideality)\n"
+"\n"
+"\n"
+"Runtime\n"
+"\n"
+"\n"
+"Amplitude\n"
+"\n"
+"\n"
+"Frequency\n"
+"\n"
+"\n"
+"Capacitance 1\n"
+"\n"
+"\n"
+"Capacitance 2\n"
+"\n"
+"\n"
+"Resistance\n"
+"\n"
+"\n"
+"Step size", nullptr));
+        pushButton->setText(QCoreApplication::translate("solver", "Run", nullptr));
+        lineEdit_7->setText(QCoreApplication::translate("solver", "0.025", nullptr));
+        lineEdit_8->setText(QCoreApplication::translate("solver", "1e-9", nullptr));
+        textEdit->setHtml(QCoreApplication::translate("solver", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">1N4148(silicon): Is = 2.52nA, nvt = 0.0438</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">1N5817(shottky): Is = 31.7uA, nvt = 0.0343</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Common values: Rt = 1, A = 3, f = 1000, C1,C2 = 2e-9, R = 1e9</p></body></html>", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("solver", "Restart", nullptr));
+        lineEdit_9->setText(QCoreApplication::translate("solver", "1e-6", nullptr));
+        label->setText(QString());
+        textBrowser->setHtml(QCoreApplication::translate("solver", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">V2 final:</p></body></html>", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class solver: public Ui_solver {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_SOLVER_H
